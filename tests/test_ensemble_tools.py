fa3601f53193ecbda2ffac08e6eaa00d55ef53dd
Duncan Leggat <ccaedle@ucl.ac.uk>
2023-12-01 14:45:36 +0000

    Add parametrization and refactor exception checks

--- tests/test_ensemble_tools.py
+++ tests/test_ensemble_tools.py
@@ -8,7 +8,6 @@ from xml.etree import ElementTree
 
 import pytest
 
-# from plugins.FabNESO.utils.make_sweep_dir
 from FabNESO.ensemble_tools import create_dict_sweep, create_dir_tree, edit_parameters
 
 
@@ -68,15 +67,29 @@ def _check_parameter_in_conditions(
     return (n_equals, n_different)
 
 
-def test_create_dir_tree(tmp_path: Path) -> None:
+@pytest.mark.parametrize("n_dirs", [1, 3, 5, 10])
+@pytest.mark.parametrize("destructive", [True, False])
+@pytest.mark.parametrize(
+    "parameter_and_range",
+    [
+        ["particle_initial_velocity", -4.0, 5.0],
+        ["particle_charge_density", 101.0, 108.0],
+        ["particle_number_density", 101.0, 108.0],
+    ],
+)
+def test_create_dir_tree(
+    tmp_path: Path,
+    *,
+    n_dirs: int,
+    destructive: bool,
+    parameter_and_range: list,
+) -> None:
     """Test the create_dir_tree method of ensemble_tools."""
     test_sweep_dir = tmp_path / "test_sweep"
-    n_dirs = 5
-    destructive = True
     copy_dir = Path("config_files") / "two_stream"
     edit_file = "conditions.xml"
-    parameter_to_scan = "particle_initial_velocity"
-    scan_range = (0.2, 5.0)
+    parameter_to_scan = parameter_and_range[0]
+    scan_range = (parameter_and_range[1], parameter_and_range[2])
     outdir_prefix = "asd"
 
     create_dir_tree(
@@ -93,40 +106,89 @@ def test_create_dir_tree(tmp_path: Path) -> None:
     assert (test_sweep_dir / "SWEEP").is_dir()
     assert len(list((test_sweep_dir / "SWEEP").iterdir())) == n_dirs
     for i in range(n_dirs):
-        cond_file = test_sweep_dir / "SWEEP" / f"asd{i}" / "conditions.xml"
+        cond_file = test_sweep_dir / "SWEEP" / f"{outdir_prefix}{i}" / "conditions.xml"
         assert cond_file.is_file()
-        para_value = scan_range[0] + (i / (n_dirs - 1)) * (
-            scan_range[1] - scan_range[0]
+        para_value = (
+            scan_range[0]
+            if n_dirs == 1
+            else scan_range[0] + (i / (n_dirs - 1)) * (scan_range[1] - scan_range[0])
         )
         assert _check_parameter_in_conditions(
             cond_file, parameter_to_scan, para_value
         ) == (1, 0)
+
+
+def _call_create_dir_tree_from_dict(input_dict: dict) -> None:
+    create_dir_tree(
+        sweep_path=input_dict["sweep_path"],
+        n_dirs=input_dict["n_dirs"],
+        destructive=input_dict["destructive"],
+        copy_dir=input_dict["copy_dir"],
+        edit_file=input_dict["edit_file"],
+        parameter_to_scan=input_dict["parameter_to_scan"],
+        scan_range=input_dict["scan_range"],
+        outdir_prefix=input_dict["outdir_prefix"],
+    )
+
+
+def test_exceptions_create_dir_tree(tmp_path: Path) -> None:
+    """Test exceptions thrown in create_dir_path."""
+    argument_dict = {
+        "sweep_path": tmp_path / "test_sweep",
+        "n_dirs": 5,
+        "destructive": True,
+        "copy_dir": Path("config_files") / "two_stream",
+        "edit_file": "conditions.xml",
+        "parameter_to_scan": "particle_initial_velocity",
+        "scan_range": (0.2, 5.0),
+        "outdir_prefix": "asd",
+    }
     # Should raise an exception when trying to edit the mesh file
-    edit_file = "mesh.xml"
+    argument_dict["edit_file"] = "mesh.xml"
     with pytest.raises(ValueError, match=r".* a CONDITIONS node.*"):
-        create_dir_tree(
-            sweep_path=test_sweep_dir,
-            n_dirs=n_dirs,
-            destructive=destructive,
-            copy_dir=copy_dir,
-            edit_file=edit_file,
-            parameter_to_scan=parameter_to_scan,
-            scan_range=scan_range,
-            outdir_prefix=outdir_prefix,
-        )
+        _call_create_dir_tree_from_dict(argument_dict)
+    # The directory already exists, so running without destructive should
+    # raise a FileExistsError
+    argument_dict["destructive"] = False
+    argument_dict["edit_file"] = "conditions.xml"
+    with pytest.raises(
+        FileExistsError, match=".*ready exists and not in destructive .*"
+    ):
+        _call_create_dir_tree_from_dict(argument_dict)
+    # fake_name.xml doesn't exist, should raise FileNotFoundError
+    argument_dict["destructive"] = True
+    argument_dict["edit_file"] = "fake_name.xml"
+    with pytest.raises(FileNotFoundError, match=".*does not exist"):
+        _call_create_dir_tree_from_dict(argument_dict)
+    # Raise TypeError if no parameter specified
+    argument_dict["edit_file"] = "conditions.xml"
+    argument_dict["parameter_to_scan"] = None
+    with pytest.raises(TypeError, match=".*parameter_to_scan not defined"):
+        _call_create_dir_tree_from_dict(argument_dict)
 
 
-def test_create_dict_sweep(tmp_path: Path) -> None:
+@pytest.mark.parametrize("n_divs", [1, 3, 5, 10])
+@pytest.mark.parametrize("destructive", [True, False])
+@pytest.mark.parametrize(
+    "parameter_dict",
+    [
+        {
+            "particle_initial_velocity": (0.1, 2.5),
+            "particle_charge_density": (102.0, 108.0),
+        },
+        {
+            "num_particles_total": (20000, 1000000),
+            "particle_number_density": (102.0, 108.0),
+        },
+    ],
+)
+def test_create_dict_sweep(
+    tmp_path: Path, *, n_divs: int, destructive: bool, parameter_dict: dict
+) -> None:
     """Test the create_dict_sweep method of ensemble_tools."""
     sweep_path = tmp_path / "test"
-    n_divs = 5
-    destructive = True
     copy_dir = Path("config_files") / "two_stream"
     edit_file = "conditions.xml"
-    parameter_dict = {
-        "particle_initial_velocity": (0.1, 2.5),
-        "particle_charge_density": (102.0, 108.0),
-    }
     create_dict_sweep(
         sweep_path=sweep_path,
         n_divs=n_divs,
@@ -149,22 +211,25 @@ def test_create_dict_sweep(tmp_path: Path) -> None:
         # Check the directory exists
         assert this_dir.is_dir()
 
-        # Check the conditions and mesh files exist
-        assert (this_dir / "conditions.xml").is_file()
-        assert (this_dir / "mesh.xml").is_file()
+        # Check that all files have been copied correctly
+        for f in copy_dir.iterdir():
+            assert (this_dir / f.name).is_file()
 
         # Check that the parameters have been edited correctly
         for i in range(len(parameter_dict)):
             parameter = list(parameter_dict.keys())[i]
             scan_range = parameter_dict[parameter]
-            para_value = scan_range[0] + (indices[i] / (n_divs - 1)) * (
-                scan_range[1] - scan_range[0]
+            para_value = (
+                scan_range[0]
+                if n_divs == 1
+                else scan_range[0]
+                + (indices[i] / (n_divs - 1)) * (scan_range[1] - scan_range[0])
             )
             assert _check_parameter_in_conditions(
                 this_dir / "conditions.xml", parameter, para_value
             ) == (1, 0)
             with pytest.raises(ValueError, match=r".*CONDITIONS.*"):
                 # The mesh file should be not edited
-                assert _check_parameter_in_conditions(
+                _check_parameter_in_conditions(
                     this_dir / "mesh.xml", parameter, para_value
                 )
