2cb6e86859b097f8bdfcf94f80eb021719323841
Duncan Leggat <ccaedle@ucl.ac.uk>
2023-12-01 16:14:30 +0000

    Updating init with warning on import error

--- FabNESO/__init__.py
+++ FabNESO/__init__.py
@@ -1,8 +1,14 @@
 """Neptune Exploratory SOftware (NESO) plugin for FabSim3."""
-import contextlib
 
-with contextlib.suppress(ImportError):
+try:
     from .tasks import neso, neso_ensemble
 
+    __all__ = ["neso", "neso_ensemble"]
 
-__all__ = ["neso", "neso_ensemble"]
+except ImportError:
+    import warnings
+
+    warnings.warn(
+        "Cannot import FabNESO tasks - fabsim.base.fab may not be importable",
+        stacklevel=2,
+    )
