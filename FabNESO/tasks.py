4818a9c75003e4b15b380c3b07b957f0711a9f38
Matt Graham <matthew.m.graham@gmail.com>
2024-01-05 17:08:34 +0000

    Add autogenerated HTML API documentation built with Sphinx (#36)
    * Add infrastructure for autogenerating Sphinx API docs
    
    * Make FabNESO.tasks module importable without FabSIM
    
    * Change tests workflow file name capitalisation for consistency

--- FabNESO/tasks.py
+++ FabNESO/tasks.py
@@ -12,13 +12,25 @@ from tempfile import TemporaryDirectory
 
 try:
     from fabsim.base import fab
+
+    fab.add_local_paths("FabNESO")
+    FAB_IMPORTED = True
 except ImportError:
-    from base import fab
+    # If FabSim not installed we create a dummy fab namespace definining placeholder
+    # decorators to make module still importable, for example when building docs
+    from types import SimpleNamespace
+
+    fab = SimpleNamespace(task=lambda f: f, load_plugin_env_vars=lambda _: lambda f: f)
+    FAB_IMPORTED = False
 
 
 from .ensemble_tools import create_dict_sweep, edit_parameters
 
-fab.add_local_paths("FabNESO")
+
+def _check_fab_module_imported() -> None:
+    if not FAB_IMPORTED:
+        msg = "fabsim.base.fab could not be imported - check FabSim3 is installed"
+        raise ImportError(msg)
 
 
 def _try_convert_to_int_and_check_positive(value: str | int, name: str) -> int:
@@ -117,6 +129,7 @@ def neso(
             ``FabNESO.ensemble_tools.edit_parameters`` to create a temporary conditions
             file with these parameter vaues overriden.
     """
+    _check_fab_module_imported()
     processes, nodes, cpus_per_process, wall_time = _check_and_process_resource_args(
         processes, nodes, cpus_per_process, wall_time
     )
@@ -195,6 +208,7 @@ def neso_ensemble(
         **parameter_scans: The set of parameters to sweep over. A colon separated list
             of lower bound, upper bound, and steps.
     """
+    _check_fab_module_imported()
     processes, nodes, cpus_per_process, wall_time = _check_and_process_resource_args(
         processes, nodes, cpus_per_process, wall_time
     )
