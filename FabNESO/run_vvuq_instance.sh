ea9848c740127d44157d13fb9d20e80d313850c8
Duncan Leggat <ccaedle@ucl.ac.uk>
2024-02-13 11:24:19 +0000

    Altering vvuq run script to not include the file copying. This is now done in a temp directory within the function

--- FabNESO/run_vvuq_instance.sh
+++ FabNESO/run_vvuq_instance.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo fabsim $1 single_run_vvuq:$PWD,solver=$4,processes=$5,nodes=$6,cpus_per_process=$7,wall_time=$8,neso_outfile=$9
+echo fabsim $1 single_run_vvuq:$PWD,mesh_file=$3,solver=$4,processes=$5,nodes=$6,cpus_per_process=$7,wall_time=$8,neso_outfile=$9
 
-pwd
-
-cp $3 .
-
-fabsim $1 single_run_vvuq:$PWD,solver=$4,processes=$5,nodes=$6,cpus_per_process=$7,wall_time=$8,neso_outfile=$9
+fabsim $1 single_run_vvuq:$PWD,mesh_file=$3,solver=$4,processes=$5,nodes=$6,cpus_per_process=$7,wall_time=$8,neso_outfile=$9
